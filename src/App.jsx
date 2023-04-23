b1f48a84a1d2092556783bd015246d2aa63c71b1
Pete Laptop <pmugasa78@gmail.com>
2023-04-23 22:36:49 +0200

    added the search by country functionality, however still need to edit the conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [dropdown, setDropDown] = useState(false);
-  const [region, setRegion] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
+  const [isFilter, setIsFilter] = useState(false);
   const [userInput, setUserInput] = useState("");
 
   //fetching data from the api
@@ -27,31 +28,27 @@ function App() {
   }, []);
 
   //searching filter
-  let result = [];
-  function search(e) {
-    e.preventDefault();
-    console.log("userInput", userInput);
-    countries.map((country) => {
-      if (country.name.common.toLowerCase().includes(userInput.toLowerCase())) {
-        return [...result, country];
-      } else {
-        return country;
-      }
-    });
-  }
 
-  countries.map((country) => {
-    console.log("countries", country.name.common);
+  const searchedCountries = countries.filter((country) => {
+    return country.name.common.toLowerCase().includes(userInput.toLowerCase());
   });
+  console.log(searchedCountries);
+  console.log(userInput);
 
   function showDropDown() {
     setDropDown(!dropdown);
   }
 
-  function filterByRegion(item) {
-    setRegion(item);
-    console.log(region);
+  //filter by region
+  function filterByRegion(region) {
+    setDropDown(!dropdown);
+    const updatedCountries = countries.filter(
+      (country) => country.region.toLowerCase() === region
+    );
+    setFilteredResults(updatedCountries);
+    setIsFilter(true);
   }
+
   if (isLoading === true) {
     return <h1>App is loading...</h1>;
   }
@@ -61,12 +58,9 @@ function App() {
       <div className="bg-very-dark-blue min-h-screen">
         <Navbar />
 
-        <form
-          className="  mt-6 mx-4 h-12 bg-dark-blue shadow-md text-white  rounded-lg "
-          onSubmit={search}
-        >
+        <div className="  mt-6 mx-4 h-12 bg-dark-blue shadow-md text-white  rounded-lg ">
           <Searchbox setUserInput={setUserInput} />
-        </form>
+        </div>
 
         {/* dropdown*/}
         <div className="relative">
@@ -74,40 +68,44 @@ function App() {
             <button className="flex items-center" onClick={showDropDown}>
               <div>Filter by Region</div>
               <div className="ml-10 mt-2">
-                <ion-icon name="chevron-down-outline" size="small"></ion-icon>
+                {dropdown === true ? (
+                  <ion-icon name="chevron-up-outline" size="small"></ion-icon>
+                ) : (
+                  <ion-icon name="chevron-down-outline" size="small"></ion-icon>
+                )}
               </div>
             </button>
           </div>
           {dropdown === true ? (
             <div className=" absolute z-10 mt-1 p-4 w-52 mx-4 flex items-center bg-dark-blue shadow-md text-white  rounded-lg">
-              <ul className="w-full">
+              <ul className="w-full space-y-2">
                 <li
-                  className="hover:bg-very-dark-blue"
+                  className="hover:bg-very-dark-blue p-2"
                   onClick={() => filterByRegion("africa")}
                 >
                   <a href="#">Africa</a>
                 </li>
                 <li
-                  className="hover:bg-very-dark-blue"
-                  onClick={() => filterByRegion("america")}
+                  className="hover:bg-very-dark-blue p-2"
+                  onClick={() => filterByRegion("americas")}
                 >
                   <a href="#">America</a>
                 </li>
                 <li
-                  className="hover:bg-very-dark-blue"
+                  className="hover:bg-very-dark-blue p-2"
                   onClick={() => filterByRegion("asia")}
                 >
                   <a href="#">Asia</a>
                 </li>
                 <li
-                  className="hover:bg-very-dark-blue"
+                  className="hover:bg-very-dark-blue p-2"
                   onClick={() => filterByRegion("europe")}
                 >
                   <a href="#">Europe</a>
                 </li>
                 <li
-                  className="hover:bg-very-dark-blue"
-                  onClick={() => filterByRegion("ocenia")}
+                  className="hover:bg-very-dark-blue p-2"
+                  onClick={() => filterByRegion("oceania")}
                 >
                   <a href="#">Oceania</a>
                 </li>
@@ -116,9 +114,17 @@ function App() {
           ) : null}
         </div>
 
-        {countries.map((country) => {
-          return <Card key={country.name.common} country={country} />;
-        })}
+        {isFilter === true
+          ? filteredResults.map((country) => {
+              return <Card key={country.name.common} country={country} />;
+            })
+          : searchedCountries
+          ? searchedCountries.map((country) => {
+              return <Card key={country.name.common} country={country} />;
+            })
+          : countries.map((country) => {
+              return <Card key={country.name.common} country={country} />;
+            })}
       </div>
     </>
   );
