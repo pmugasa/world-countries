186829c6bc21fb8ff66e1598bd895a34394bfbbc
Pete Laptop <pmugasa78@gmail.com>
2023-04-23 14:56:24 +0200

    added the card structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  //fetching data from the api
+  useEffect(() => {
+    function fetchData() {
+      axios
+        .get("https://restcountries.com/v3.1/all")
+        .then((response) => {
+          console.log("name", response.data[20].name.common);
+          console.log("capital", response.data[20].capital);
+          console.log("region", response.data[20].region);
+          console.log("population", response.data[20].population);
+          console.log("flag", response.data[20].flags.png);
+          setCountries(response.data);
+        })
+        .catch((error) => alert(error.message));
+    }
+
+    fetchData();
+  }, []);
+
+  if (countries.length > 0) {
+    countries.forEach((country) => {
+      console.log(country);
+    });
+  } else {
+    console.log("loading...");
+  }
+
   return (
     <>
       <div className="bg-very-dark-blue min-h-screen">
@@ -8,7 +40,7 @@ function App() {
           </h1>
         </div>
 
-        <form className="  mt-4 mx-4 h-12 bg-dark-blue shadow-md text-white  rounded-lg ">
+        <form className="  mt-6 mx-4 h-12 bg-dark-blue shadow-md text-white  rounded-lg ">
           <div className="relative flex items-center w-full h-full">
             <div className="absolute flex items-center pl-4">
               <ion-icon
@@ -24,6 +56,56 @@ function App() {
             />
           </div>
         </form>
+
+        {/* dropdown*/}
+        <div className="relative">
+          <div className="container  mt-6 p-4 w-52 mx-4 h-12 flex items-center bg-dark-blue shadow-md text-white  rounded-lg">
+            <button className="flex items-center">
+              <div>Filter by Region</div>
+              <div className="ml-10 mt-2">
+                <ion-icon name="chevron-down-outline" size="small"></ion-icon>
+              </div>
+            </button>
+          </div>
+          <div className=" absolute z-10 mt-1 p-4 w-52 mx-4 flex items-center bg-dark-blue shadow-md text-white  rounded-lg">
+            <ul className="w-full">
+              <li className="hover:bg-very-dark-blue">
+                <a href="#">Africa</a>
+              </li>
+              <li className="hover:bg-very-dark-blue">
+                <a href="#">America</a>
+              </li>
+              <li className="hover:bg-very-dark-blue">
+                <a href="#">Asia</a>
+              </li>
+              <li className="hover:bg-very-dark-blue">
+                <a href="#">Europe</a>
+              </li>
+              <li className="hover:bg-very-dark-blue">
+                <a href="#">Oceania</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        {/*card*/}
+        <div className="container mt-10  bg-dark-blue text-sm text-white w-72 h-80  mx-auto rounded-lg shadow-3xl shadow-black">
+          <div className="img container ">
+            <img src="https://flagcdn.com/w320/bs.png" alt="country-flag" />
+          </div>
+          <div className="text container p-4 space-y-4">
+            <h2 className="font-extrabold text-base">Germany</h2>
+            <p className="font-semibold text-sm mt-6">
+              Population: <span className="font-light text-sm">81,770,900</span>
+            </p>
+            <p className="font-semibold text-sm">
+              Region: <span className="font-light text-sm">Europe</span>
+            </p>
+            <p className="font-semibold text-sm">
+              Capital: <span className="font-light text-sm">Berlin</span>
+            </p>
+          </div>
+        </div>
       </div>
     </>
   );
